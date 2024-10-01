497e2bdacc08b79b31497d11e52ae10f03cfe3dc
SneSta3 <snjezastakic@gmail.com>
2024-10-01 12:10:55 +0200

    prvi cas

--- 0-komentari-promenljive.py
+++ 0-komentari-promenljive.py
@@ -1,3 +1,47 @@
+# komentari
 print ('komentari i promenljive')
 
+# varijable/promenljive 
+naslov = 'varijable'
+print(naslov)
+ime = 'Freeda'
+godine = 4
+print('moj psic se zove ' + ime + '  i ima ' + str(godine) + ' godine')
+
+broj1 = 100
+broj2 = 200
+print(broj1 + broj2)
+
+broj1 = 100
+broj2 = '200'
+print(broj1 + int(broj2))
+
+x = 100
+x = 'snjeza'
+print(x)
+
+a, b, c = 30, 390, 3009
+print(b)
+
+x = y = z = 200
+print(x, y, z)
+
+ime, prezime, email = 'Snjeza ', 'Stakic ', 'snjezastakic@gmail.com'
+print(ime, prezime, email)
+
+auto = 'mercedes'
+AUTO = 'BMW'
+print(auto)
+print(AUTO)
+
+first_name = 'snjeza'
+firstName = 'sneza'
+_first_name = 'snjeza'
+name_1 = 'snjeza'
+
+# 1name = 'snjeza'
+# first-name = 'snjeza'
+# !name = 'snjeza'
+# first+name = 'snjeza'
+# first name = 'snjeza'
 
